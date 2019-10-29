5b1f5df39d9b0a2010b8cb68d97966bde5896656
vedamsh <vedamsh.vanja@st.ovgu.de>
2019-10-29 18:31:02 +0100

    created a new BoardTest class ( board and nullboard)

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -2,35 +2,44 @@ package nl.tudelft.jpacman.board;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+/**
+ * Test class to construct board with out null.
+ *
+ */
 public class BoardTest {
 
+
+    /**
+     * it constructs a board with 1x1 grid.
+     */
     @Test
-    void board(){
+    void board() {
 
         BasicSquare basicSquare = new BasicSquare();
-        Square[][] squares= {{basicSquare}};
+        Square[][] squares = {{basicSquare}};
 
         Board board = new Board(squares);
-        Square sq0 = board.squareAt(0,0);
+        Square sq0 = board.squareAt(0, 0);
         assertThat(board).isInstanceOf(Board.class);
 
     }
 
-    @Test
+   /** @Test
     void Nullboard(){
 
         BasicSquare basicsquare2 = null;
         Square[][] squares2= {{basicsquare2}};
 
         Board board2 = new Board(squares2);
-        Square sq1 = board2.squareAt(0,0);
+        Square sq1 = board2.squareAt(0 ,0);
 
 
 
     }
-
+**/
 }
