0c6f5b9e38af895f4d139761fecc18c02c086904
vedamsh <vedamsh.vanja@st.ovgu.de>
2019-11-19 23:44:48 +0100

    instantiate GhostmapParser

---
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/Clydetest.java
@@ -0,0 +1,33 @@
+package nl.tudelft.jpacman.npc.ghost;
+
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.board.Square;
+import nl.tudelft.jpacman.level.LevelFactory;
+import nl.tudelft.jpacman.level.MapParser;
+import nl.tudelft.jpacman.npc.Ghost;
+import nl.tudelft.jpacman.points.DefaultPointCalculator;
+import nl.tudelft.jpacman.points.PointCalculator;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+
+public class Clydetest {
+        private GhostMapParser ghostMapParser;
+        private PacManSprites pacManSprites = new PacManSprites() ;
+
+    public void InstatiateGhostMapParser(){
+
+            GhostFactory ghostFactory = new GhostFactory(pacManSprites);
+            BoardFactory boardFactory = new BoardFactory(pacManSprites);
+        PointCalculator pointCalculator = new DefaultPointCalculator();
+        LevelFactory levelFactory = new LevelFactory(pacManSprites,ghostFactory,pointCalculator);
+        ghostMapParser = new GhostMapParser(levelFactory,boardFactory,ghostFactory);
+
+        }
+
+
+
+
+
+
+
+    }
+}
