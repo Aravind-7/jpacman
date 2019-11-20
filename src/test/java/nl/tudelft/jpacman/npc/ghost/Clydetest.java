44bdb362e88c403e80a93fb2f2c6fa26f24cb506
vedamsh <vedamsh.vanja@st.ovgu.de>
2019-11-20 11:39:19 +0100

    Test For NoShyness

--- src/test/java/nl/tudelft/jpacman/npc/ghost/Clydetest.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/Clydetest.java
@@ -9,60 +9,55 @@ import nl.tudelft.jpacman.level.PlayerFactory;
 import nl.tudelft.jpacman.points.DefaultPointCalculator;
 import nl.tudelft.jpacman.points.PointCalculator;
 import nl.tudelft.jpacman.sprite.PacManSprites;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 /**
  * here we create a Class to test the movement of the C'lyde Ghost.
  */
 public class Clydetest {
-        private GhostMapParser ghostMapParser;
-        private PacManSprites pacManSprites = new PacManSprites() ;
 
-        /*
-        we instatiate the GhostMapParser and also give the required factories.
-         */
+    private PacManSprites pacManSprites = new PacManSprites();
+    private GhostFactory ghostFactory = new GhostFactory(pacManSprites);
+    private BoardFactory boardFactory = new BoardFactory(pacManSprites);
+    private PointCalculator pointCalculator = new DefaultPointCalculator();
+    private LevelFactory levelFactory = new LevelFactory(pacManSprites,
+        ghostFactory, pointCalculator);
+    private GhostMapParser ghostMapParser = new GhostMapParser(levelFactory,
+        boardFactory, ghostFactory);
 
-    public void InstatiateGhostMapParser(){
+    /**
+     * Good weather case.
+     *Here we create a test case that the pacman is far away from the clyde by more than 8.
+     * And the clyde tries to move in the direction of pacman.
+     */
+    @Test
+        void no_Shyness() {
 
-            GhostFactory ghostFactory = new GhostFactory(pacManSprites);
-            BoardFactory boardFactory = new BoardFactory(pacManSprites);
-
-        PointCalculator pointCalculator = new DefaultPointCalculator();
-        LevelFactory levelFactory = new LevelFactory(pacManSprites,ghostFactory,pointCalculator);
-        ghostMapParser = new GhostMapParser(levelFactory,boardFactory,ghostFactory);
-
-        }
-     /*
-     * we write a test case , when the pacman is  far away from more than 8 places, the Clyde tries to go towards the direction of pacman.
-      */
-        @Test
-    void NoShyness(){
-
-            List<String > list = Arrays.asList(
+        List<String> position = Arrays.asList(
                 "############",
-                "#P         C#",
-                "#############"
-            );
-            Level level = ghostMapParser.parseMap(list);
+                "#P        C#",
+                "############"
+        );
+        Level level = ghostMapParser.parseMap(position);
 
-            PlayerFactory playerFactory = new PlayerFactory(pacManSprites);
-            Player p = playerFactory.createPacMan();
-            level.registerPlayer(p);
-            p.setDirection(Direction.EAST);
+        PlayerFactory playerFactory = new PlayerFactory(pacManSprites);
+        Player p = playerFactory.createPacMan();
+        level.registerPlayer(p);
+        p.setDirection(Direction.WEST);
 
-            Clyde c = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
 
-            assertThat(c.nextAiMove()).isEqualTo(Optional.of(Direction.EAST));
-
-        }
+        assert clyde != null;
+        assertEquals(Optional.of(Direction.WEST), clyde.nextAiMove());
 
     }
+}
+
 
