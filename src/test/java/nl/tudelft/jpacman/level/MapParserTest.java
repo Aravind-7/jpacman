5b4cf2200fa11189e8e5852a0d19cbba955ebefc
Elf-OrderBy <azima.fariha@st.ovgu.de>
2019-12-18 18:36:12 +0100

    nice weather test for MapParser test - exercise 20

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -6,44 +6,31 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mockito.configuration.IMockitoConfiguration;
-import nl.tudelft.jpacman.level.LevelFactory;
 
-import java.util.Map;
-
+/**
+ * create a test class.
+ */
 public class MapParserTest{
-
     private final LevelFactory levelCreator = Mockito.mock(LevelFactory.class);
     private final BoardFactory boardCreator = Mockito.mock(BoardFactory.class);
     private final Square wall = Mockito.mock(Square.class);
     private final Square ground = Mockito.mock(Square.class);
-
     private MapParser mapParser;
-
-
+    /**
+     * create a before each.
+     */
     @BeforeEach
     void setup(){
         mapParser = new MapParser(levelCreator,boardCreator);
         Mockito.when(boardCreator.createGround()).thenReturn(ground);
         Mockito.when(boardCreator.createWall()).thenReturn(wall);
-
     }
 /**
- *
+ *test to verify the ground.
  */
-     @Test
+    @Test
     void forground(){
-
-
          mapParser.parseMap(Lists.newArrayList( " "));
          Mockito.verify(boardCreator).createGround();
-
      }
-
-
-
-
-
-
-
 }
