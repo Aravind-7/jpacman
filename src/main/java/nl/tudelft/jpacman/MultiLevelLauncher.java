66d62847040674d8c16dfdf482b4a77cae12a7e4
vedamsh Vanja <vedamsh.vanja@st.ovgu.de>
2020-01-22 21:56:09 +0100

    Final Commit

--- src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
+++ src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -8,7 +8,6 @@ import nl.tudelft.jpacman.points.DefaultPointCalculator;
 public class MultiLevelLauncher extends Launcher {
 
     private MultiLevelGame multiGame;
-    private String levelMap = DEFAULT_MAP;
 
 
     /**
@@ -39,9 +38,4 @@ public class MultiLevelLauncher extends Launcher {
         return multiGame;
     }
 
-    public Launcher withMapFile(String fileName) {
-        levelMap = fileName;
-        return this;
-
-    }
 }
