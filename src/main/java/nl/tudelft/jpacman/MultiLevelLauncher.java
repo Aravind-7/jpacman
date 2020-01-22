45eb7c7f0ce275dc91e51e5cb7a7515021075497
vedamsh Vanja <vedamsh.vanja@st.ovgu.de>
2020-01-22 20:22:28 +0100

    Exercise 45,46

---
+++ src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -0,0 +1,40 @@
+package nl.tudelft.jpacman;
+
+import nl.tudelft.jpacman.game.MultiLevelGame;
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.points.DefaultPointCalculator;
+
+public class MultiLevelLauncher extends Launcher {
+
+    private MultiLevelGame multiGame;
+
+
+    /**
+     * This method is to create a new launcher for multilevel game.
+     * @return it returns a multiGame.
+     */
+    @Override
+    public MultiLevelGame makeGame() {
+        Player player = getPlayerFactory().createPacMan();
+        multiGame = new MultiLevelGame(player, new DefaultPointCalculator());
+        addLevels();
+        return multiGame;
+    }
+
+    private void addLevels() {
+        addLevel(makeLevel());
+        addLevel(makeLevel());
+        addLevel(makeLevel());
+    }
+
+    private void addLevel(Level newLevel) {
+       multiGame.addLevel(newLevel);
+    }
+
+    @Override
+    public MultiLevelGame getGame() {
+        return multiGame;
+    }
+
+}
---
+++ src/main/java/nl/tudelft/jpacman/game/MultiLevelGame.java
@@ -0,0 +1,49 @@
+package nl.tudelft.jpacman.game;
+
+import com.google.common.collect.ImmutableList;
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.points.PointCalculator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MultiLevelGame extends Game {
+
+
+    private final Player player;
+    private List<Level> levels = new LinkedList<>();
+    private int levelnumber =0 ;
+
+    public MultiLevelGame(Player player, PointCalculator pointCalculator) {
+
+        super(pointCalculator);
+        this.player = player;
+        levels = new LinkedList<Level>();
+    }
+
+
+    public void addLevel(Level level) {
+        levels.add(level);
+    }
+
+
+    @Override
+    public Level getLevel() {
+        return levels.get(levelnumber);
+
+    }
+    @Override
+    public void levelWon(){this.levelnumber++;
+
+    }
+
+
+    @Override
+    public List<Player> getPlayers() {
+        return ImmutableList.of(player);
+
+    }
+}
+
+
